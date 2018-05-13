a907b469cb765c79d044c7c5c3e2e1b0539abba0
BarishNamazov <barish2003@gmail.com>
2018-05-13 21:04:51 +0400

    Add files via upload

---
+++ cluedo.cpp
@@ -0,0 +1,12 @@
+#include "grader.h"
+#include "cluedo.h"
+
+void Solve(){
+    int a[4] = {0, 1, 1, 1};
+    int res = Theory(a[1], a[2], a[3]);
+    while (res > 0) {
+        a[res]++;
+        res = Theory(a[1], a[2], a[3]);
+    }
+    return;
+}
